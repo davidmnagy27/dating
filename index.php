bc515c9e97a2656eb7d9f89cdb50f7fe39c66610
David Nagy <davidmnagy27@gmail.com>
2019-04-19 22:20:38 -0400

    route personal info

--- index.php
+++ index.php
@@ -1,20 +1,26 @@
 <?php
+session_start();
 
-//Turn on error reporting
-ini_set('display_errors', 1);
+
+
+
+//TUrn on error reporting
+ini_set('display_errors', true);
 error_reporting(E_ALL);
-
-//require the autoload file
+//Require autoload file
 require_once('vendor/autoload.php');
-
 //Create an instance of the Base class
 $f3 = Base::instance();
-
-//Define a default route
-$f3->route('GET /', function()
-{
-    echo'<h1>Hello World!</h1>';
+//define a default route
+$f3->route('GET /', function () {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+$f3->route('POST /personal-info', function () {
+    $view = new Template();
+    echo $view->render('views/person-info.html');
 });
 
-//Run fat free
+//Run fat-free
 $f3->run();
+?>
